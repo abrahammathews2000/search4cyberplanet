0206c1b1aba032d78b3108ded5b9cffd5b1879da
abrahammathews2000 <abrahammathews2000@gmail.com>
2023-06-06 21:16:59 +0530

    Uploading rest of the total 1200 lightcurves and shapes

--- lightCurveGenerator/LC_list_generator_2.py
+++ lightCurveGenerator/LC_list_generator_2.py
@@ -12,7 +12,8 @@ import os
 from LC_list_generator import generate_lc_dict
 
 # generate_lc_dict(rad = rad_list[typeShape],edgy = edgy_list[typeShape],noEdges = noEdges_list[typeShape], descpType = typeShape,noVariety = 2)
-rad_array = np.linspace(0.,1.,5)
+
+rad_array = np.linspace(0.,1.,5) #[0.   0.25 0.5  0.75 1.  ] Give it manually
 edgy_array = np.linspace(0,9,10)
 edges = np.array([3,4,5,6,7,8,9,10])
 variety = 3
@@ -21,7 +22,7 @@ print("rad_array = ",rad_array)
 print("edgy_array = ",edgy_array)
 print("edges = ",edges)
 print("File name rad_edgy_noEdges_variety")
-for i in rad_array:
-    for j in edgy_array:
-        for k in edges:
-            generate_lc_dict(rad=i,edgy=j,noEdges=k,noVariety=variety)
+# for i in rad_array: (Due to crashing rad is given manually)
+for j in edgy_array:
+    for k in edges:
+        generate_lc_dict(rad=1,edgy=j,noEdges=k,noVariety=variety)
